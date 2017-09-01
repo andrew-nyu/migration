aaf47f68db22f211aab16d5657e4da88062212d4
andrew-nyu <andrew.reid.bell@nyu.edu>
2017-09-01 12:03:12 -0400

    Add files via upload

--- mcScriptRun.m
+++ mcScriptRun.m
@@ -4,26 +4,58 @@ outputList = {};
 modelRuns = 100;
 
 %define the levels and parameters you will explore, as below
-parameterNames = {  'modelParameters.numAgents'; ...
-    'networkParameters.networkDistanceSD'};
-parameterLimits = { [1000 5000]; ...
-    [3 15]};
-roundValYN = [1 0];
+mcParams = table([],[],[],[],'VariableNames',{'Name','Lower','Upper','RoundYN'});
+
+mcParams = [mcParams; {'modelParameters.numAgents', 1000, 5000, 1}];
+
+mcParams = [mcParams; {'modelParameters.incomeInterval', 1, 4, 1}];
+mcParams = [mcParams; {'networkParameters.networkDistanceSD', 5, 15, 1}];
+mcParams = [mcParams; {'networkParameters.connectionsMean', 1, 5, 1}];
+mcParams = [mcParams; {'networkParameters.connectionsSD', 1, 3, 1}];
+mcParams = [mcParams; {'agentParameters.incomeShareFractionMean', 0.2, 0.6, 0}];
+mcParams = [mcParams; {'agentParameters.incomeShareFractionSD', 0, 0.2, 0}];
+mcParams = [mcParams; {'agentParameters.shareCostThresholdMean', 0.2, 0.4, 0}];
+mcParams = [mcParams; {'agentParameters.shareCostThresholdSD', 0, 0.2, 0}];
+mcParams = [mcParams; {'agentParameters.interactMean', 0.2, 0.6, 0}];
+mcParams = [mcParams; {'agentParameters.interactSD', 0, 0.2, 0}];
+mcParams = [mcParams; {'agentParameters.randomLearnMean', 0, 1, 0}];
+mcParams = [mcParams; {'agentParameters.randomLearnSD', 0, 0.2, 0}];
+mcParams = [mcParams; {'agentParameters.randomLearnCountMean', 1, 3, 1}];
+mcParams = [mcParams; {'agentParameters.randomLearnCountSD', 0, 2, 1}];
+mcParams = [mcParams; {'agentParameters.chooseMean', 0.2, 0.6, 0}];
+mcParams = [mcParams; {'agentParameters.chooseSD', 0, 0.2, 0}];
+mcParams = [mcParams; {'agentParameters.knowledgeShareFracMean', 0.05, 0.2, 0}];
+mcParams = [mcParams; {'agentParameters.knowledgeShareFracSD', 0, 0.05, 0}];
+mcParams = [mcParams; {'agentParameters.bestLocationMean', 1, 3, 1}];
+mcParams = [mcParams; {'agentParameters.bestLocationSD', 0, 1, 1}];
+mcParams = [mcParams; {'agentParameters.bestPortfolioMean', 1, 3, 1}];
+mcParams = [mcParams; {'agentParameters.bestPortfolioSD', 0, 1, 1}];
+mcParams = [mcParams; {'agentParameters.randomLocationMean', 1, 3, 1}];
+mcParams = [mcParams; {'agentParameters.randomLocationSD', 0, 1, 1}];
+mcParams = [mcParams; {'agentParameters.randomPortfolioMean', 1, 3, 1}];
+mcParams = [mcParams; {'agentParameters.randomPortfolioSD', 0, 1, 1}];
+mcParams = [mcParams; {'agentParameters.numPeriodsEvaluateMean', 12, 24, 1}];
+mcParams = [mcParams; {'agentParameters.numPeriodsEvaluateSD', 0, 6, 1}];
+mcParams = [mcParams; {'agentParameters.numPeriodsMemoryMean', 12, 24, 1}];
+mcParams = [mcParams; {'agentParameters.numPeriodsMemorySD', 0, 6, 1}];
+mcParams = [mcParams; {'agentParameters.discountRateMean', 0.02, 0.1, 0}];
+mcParams = [mcParams; {'agentParameters.discountRateSD', 0, 0.02, 0}];
+mcParams = [mcParams; {'agentParameters.rValueMean', 0.5, 1, 0}];
+mcParams = [mcParams; {'agentParameters.rValueSD', 0, 0.2, 0}];
 
 %make the full factorial design
 
 for indexI = 1:modelRuns
-    experiment = dataset([],[],'VarNames',{'parameterNames','parameterValues'});
-    for indexJ = 1:(size(parameterNames))
-        tempName = parameterNames{indexJ};
-        tempMin = parameterLimits{indexJ}(1);
-        tempMax = parameterLimits{indexJ}(2);
+    experiment = table([],[],'VariableNames',{'parameterNames','parameterValues'});
+    for indexJ = 1:(height(mcParams))
+        tempName = mcParams.Name{indexJ};
+        tempMin = mcParams.Lower(indexJ);
+        tempMax = mcParams.Upper(indexJ);
         tempValue = tempMin + (tempMax-tempMin) * rand();
-        if(roundValYN(indexJ))
+        if(mcParams.RoundYN(indexJ))
             tempValue = round(tempValue);
         end
-        newRun = dataset({tempName}, tempValue, 'VarNames',{'parameterNames','parameterValues'});
-        experiment = vertcat(experiment, newRun);
+        experiment = [experiment;{tempName, tempValue}];
     end
     experimentList{indexI} = experiment;
 end
