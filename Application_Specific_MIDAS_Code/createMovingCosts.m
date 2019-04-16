44fc0502ebb8d7dfcdea04aaf7bb070eedc4f0c6
balbionthebox <stef.balbi@gmail.com>
2019-04-16 18:45:00 +0200

    6 regions map is generated but squeezed

--- Application_Specific_MIDAS_Code/createMovingCosts.m
+++ Application_Specific_MIDAS_Code/createMovingCosts.m
@@ -21,7 +21,7 @@ function [movingCosts ] = createMovingCosts(locations, distanceMatrix, mapParame
 
 %   Specify something similar for moving costs between any two spots
 
-baseMovingCosts = [ %100000; ... %same district
+baseMovingCosts = [ 100000; ... %same district
     %200000; ... %same state, different district
     %500000; ... %same country, different state
     80000000]; %different country
