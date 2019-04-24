5949ed3c3fdf0de9e01069a0b40da3e7601f6e45
balbionthebox <stef.balbi@gmail.com>
2019-04-24 18:02:47 +0200

    cherry pick of memory change

--- Application_Specific_MIDAS_Code/updateHistory.m
+++ Application_Specific_MIDAS_Code/updateHistory.m
@@ -1,23 +1,20 @@
-function utilityHistory = updateHistory(utilityVariables, modelParameters, indexT, countAgentsPerLayer)
+function utilityVariables = updateHistory(utilityVariables, modelParameters, indexT, countAgentsPerLayer)
 
 %income functions are of the form f(k,m,nExpected,n_actual, base)
 % - note that this may change depending on the simulation -
 %be sure that whatever your income functions are, the cellfun input
 %matches appropriately
-utilityHistory = utilityVariables.utilityHistory;
+
+onesList = ones(size(utilityVariables.utilityHistory,1),1);
 
 
-
-onesList = ones(size(utilityHistory,1),1);
-
-for indexL = 1:size(utilityHistory,2)
-    utilityHistory(:,indexL, indexT) = arrayfun(utilityVariables.utilityLayerFunctions{indexL}, ...
+for indexL = 1:size(utilityVariables.utilityHistory,2)
+    utilityVariables.utilityHistory(:,indexL, indexT) = arrayfun(utilityVariables.utilityLayerFunctions{indexL}, ...
         onesList*modelParameters.utility_k, ...
         onesList*modelParameters.utility_m, ...
         onesList*utilityVariables.nExpected(indexL), ...
         countAgentsPerLayer(:,indexL, indexT), ...
         utilityVariables.utilityBaseLayers(:,indexL,indexT));
 end
-
 
 end
